6546e818173c35a57dd19e359e0592c5c2a53b6b
Renata Riboli <renata_riboli@hotmail.com>
2024-10-15 16:59:46 -0300

    adicionado comentários sobre o código

--- index.js
+++ index.js
@@ -5,21 +5,34 @@ let tasks = [
 ]
 
 const getCheckboxInput = ({ id, description, checked}) => {
-    const checkbox = document.createElement('input');
+    const checkbox = document.createElement('input'); //Define o type como 'checkbox', o id com base no id da tarefa (para garantir que o checkbox e o label fiquem conectados), e o atributo checked é definido com base no valor booleano passado (se checked for true, a caixa estará marcada).
     const label = document.createElement('label');
     const wrapper = document.createElement('div'); //une em uma div aa conts checkbox e label
+    const checkboxId = `${id}-checkbox`
 
     checkbox.type = 'checkbox'; 
-    checkbox.id = `${id}-checkbox`;
-    checkbox.checked = task.checked; //se checked for true, checkbox marcada
+    checkbox.id = checkboxId
+    checkbox.checked = checked; //se checked for true, checkbox marcada
+
+    label.textContent = description;
+    label.htmlFor = checkboxId; 
+
+    wrapper.className = 'checkbox-label-container'
+
+    wrapper.appendChild(checkbox);
+    wrapper.appendChild(label);
+
+    return wrapper;
 }
 
 window.onload = function() { //assim que a janela carregar = window.onload
     tasks.forEach((task) => {
+        const checkbox = getCheckboxInput(task);
         const list = document.getElementById('to-do-list');
         const toDo = document.createElement('li');
 
-        toDo.textContent = task.description;
+        toDo.id = task.id;
+        toDo.appendChild(checkbox);
 
         list.appendChild(toDo);
     })
